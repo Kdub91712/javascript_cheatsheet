241dbe57951479dceb7accbfdeb40e6b10467e87
Kevin Wilson <kev1583@gmail.com>
2019-07-10 11:43:57 -0500

    adding style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 
 let factorial = (value) => {
 
@@ -44,43 +44,119 @@ let sortArrayNumbersLargestToSmallest = (value) => {
 
 }
 
+let iterateOverArray = (value) => {
+
+  let newArray = []
+  value.map((item, index) => 
+    newArray.push(item)
+  )
+
+  return newArray.join();
+}
+
+let addToBeginningOfArray = (value) => {
+
+  value.unshift('test');
+  return value.join();
+
+}
+
+let addToEndOfArray = (value) => {
+
+  value.push('test')
+  return value.join();
+
+}
+
+let removeFirstElementFromArray = (value) => {
+
+  value.shift();
+  return value.join();
+
+}
+
+let removeLastElementFromArray = (value) => {
+
+  value.pop();
+  return value.join();
+
+}
+
 function App() {
 
   return (
-    <>
-      <div className="App">
-          Value: 5
-          Factorial: <div>{factorial(5)}</div>
+    <div className="practice">
+
+      <div className="example">
+
+          Factorial
+          <div>Value: 5</div>
+          <div>Answer: {factorial(5)}</div>
       </div>
 
       <div className="example">
-        <br></br>
+
         Sort by largest in array:
         <div>Value: ["We", "I", "Them", "You"]</div>
         <div>Answer: {sortByAlphabeticalAscInArray(["We", "I", "Them", "You"])}</div>
       </div>
       
       <div className="example">
-        <br></br>
+
         Sort by smallest in array: 
         <div>Value: ["We", "I", "Them", "You"]</div>
         <div>Answer: {sortByAlphabeticalDscInArray(["We", "I", "Them", "You"])}</div>
       </div>
 
       <div className="example">
-        <br></br>
+
         Sort array numbers smallest to largest: 
         <div>Value: [10, 5, 100, 25]</div>
         <div>Answer: {sortArrayNumbersSmallestToLargest([10, 5, 100, 25])}</div>
       </div>
 
       <div className="example">
-        <br></br>
+
         Sort array numbers largest to smallest: 
         <div>Value: [10, 5, 100, 25]</div>
         <div>Answer: {sortArrayNumbersLargestToSmallest([10, 5, 100, 25])}</div>
       </div>
-    </>
+
+      <div className="example">
+
+        Iterate over array: 
+        <div>Value: [10, 5, 100, 25]</div>
+        <div>Answer: {iterateOverArray([10, 5, 100, 25])}</div>
+      </div>
+
+      <div className="example">
+
+        Add to beginning of array: 
+        <div>Value: [10, 5, 100, 25]</div>
+        <div>Answer: {addToBeginningOfArray([10, 5, 100, 25])}</div>
+      </div>
+
+      <div className="example">
+
+        Add to End of array: 
+        <div>Value: [10, 5, 100, 25]</div>
+        <div>Answer: {addToEndOfArray([10, 5, 100, 25])}</div>
+      </div>
+
+      <div className="example">
+
+        Remove first element from array: 
+        <div>Value: [10, 5, 100, 25]</div>
+        <div>Answer: {removeFirstElementFromArray([10, 5, 100, 25])}</div>
+      </div>
+
+      <div className="example">
+
+        Remove last element from array: 
+        <div>Value: [10, 5, 100, 25]</div>
+        <div>Answer: {removeLastElementFromArray([10, 5, 100, 25])}</div>
+      </div>
+    </div>
   );
 }
 
