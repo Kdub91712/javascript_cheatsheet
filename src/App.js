32dec620292303eda73fa0f73dfd761628da7bb4
Kevin Wilson <kev1583@gmail.com>
2019-07-10 11:19:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+let factorial = (value) => {
+
+  if (value === 0) {
+    return 1;
+  }
+
+  return value * factorial(value - 1);
+
+}
+
+let sortByAlphabeticalAscInArray = (value) =>  {
+
+  return value.sort().join();
+
+}
+
+let sortByAlphabeticalDscInArray = (value) =>  {
+
+  value.sort();
+  return value.reverse().join();
+
+}
+
+let sortArrayNumbersSmallestToLargest = (value) => {
+
+  value.sort(function(a, b) {
+    return a - b;
+  })
+
+  return value.join();
+
+}
+
+let sortArrayNumbersLargestToSmallest = (value) => {
+
+  value.sort(function(a, b) {
+    return b - a;
+  })
+
+  return value.join();
+
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+          Value: 5
+          Factorial: <div>{factorial(5)}</div>
+      </div>
+
+      <div className="example">
+        <br></br>
+        Sort by largest in array:
+        <div>Value: ["We", "I", "Them", "You"]</div>
+        <div>Answer: {sortByAlphabeticalAscInArray(["We", "I", "Them", "You"])}</div>
+      </div>
+      
+      <div className="example">
+        <br></br>
+        Sort by smallest in array: 
+        <div>Value: ["We", "I", "Them", "You"]</div>
+        <div>Answer: {sortByAlphabeticalDscInArray(["We", "I", "Them", "You"])}</div>
+      </div>
+
+      <div className="example">
+        <br></br>
+        Sort array numbers smallest to largest: 
+        <div>Value: [10, 5, 100, 25]</div>
+        <div>Answer: {sortArrayNumbersSmallestToLargest([10, 5, 100, 25])}</div>
+      </div>
+
+      <div className="example">
+        <br></br>
+        Sort array numbers largest to smallest: 
+        <div>Value: [10, 5, 100, 25]</div>
+        <div>Answer: {sortArrayNumbersLargestToSmallest([10, 5, 100, 25])}</div>
+      </div>
+    </>
   );
 }
 
