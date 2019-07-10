d88d60a22193ef8896b5fed7ba74bcc2abef6583
Kevin Wilson <kev1583@gmail.com>
2019-07-10 13:19:23 -0500

    adding show code feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 let factorial = (value) => {
@@ -89,6 +89,19 @@ let addItemToObject = (value) => {
 
 }
 
+let iterateOverObject = (value) => {
+
+  let newArray = [];
+  Object.getOwnPropertyNames(value).map((key) => 
+
+    newArray.push(value[key])
+
+  );
+
+  return newArray.join();
+
+}
+
 let removeItemFromObject = (value) => {
 
   delete value.first;
@@ -96,107 +109,227 @@ let removeItemFromObject = (value) => {
 
 }
 
-function App() {
+export default class App extends Component {
 
-  return (
-    <div className="practice">
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCode: false
+    }
 
-      <div className="category">
-        Recursion
-        <div className="example">
+  }
 
-            Factorial
-            <div>Value: 5</div>
-            <div>Answer: {factorial(5)}</div>
+  componentDidMount() {
+
+  }
+
+  clickHandler = () => {
+    this.setState({
+      showCode: !this.state.showCode
+    })
+  }
+
+  render() {
+    return (
+      <div className="practice">
+
+        <div className="category">
+          Recursion
+          <div className="example">
+
+              Factorial
+              <div>Value: 5</div>
+              <div>Answer: {factorial(5)}</div>
+
+              {this.state.showCode &&
+                <div className="code">
+                <div>Code:</div>
+                <code>{factorial.toString()}</code>
+              </div>
+            }
+          </div>
+        </div>
+
+        <div className="category">
+          Arrays
+          <div className="example">
+
+            Sort by largest in array:
+            <div>Value: ["We", "I", "Them", "You"]</div>
+            <div>Answer: {sortByAlphabeticalAscInArray(["We", "I", "Them", "You"])}</div>
+            {this.state.showCode &&
+                <div className="code">
+                <div>Code:</div>
+                <code>{sortByAlphabeticalAscInArray.toString()}</code>
+              </div>
+            }
+            
+          </div>
+          
+          <div className="example">
+
+            Sort by smallest in array: 
+            <div>Value: ["We", "I", "Them", "You"]</div>
+            <div>Answer: {sortByAlphabeticalDscInArray(["We", "I", "Them", "You"])}</div>
+
+            {this.state.showCode &&
+                <div className="code">
+                <div>Code:</div>
+                <code>{sortByAlphabeticalDscInArray.toString()}</code>
+              </div>
+            }
+          </div>
+
+          <div className="example">
+
+            Sort array numbers smallest to largest: 
+            <div>Value: [10, 5, 100, 25]</div>
+            <div>Answer: {sortArrayNumbersSmallestToLargest([10, 5, 100, 25])}</div>
+
+            {this.state.showCode &&
+                <div className="code">
+                <div>Code:</div>
+                <code>{sortArrayNumbersSmallestToLargest.toString()}</code>
+              </div>
+            }
+          </div>
+
+          <div className="example">
+
+            Sort array numbers largest to smallest: 
+            <div>Value: [10, 5, 100, 25]</div>
+            <div>Answer: {sortArrayNumbersLargestToSmallest([10, 5, 100, 25])}</div>
+
+            {this.state.showCode &&
+                <div className="code">
+                <div>Code:</div>
+                <code>{sortArrayNumbersLargestToSmallest.toString()}</code>
+              </div>
+            }
+          </div>
+
+          <div className="example">
+
+            Iterate over array: 
+            <div>Value: [10, 5, 100, 25]</div>
+            <div>Answer: {iterateOverArray([10, 5, 100, 25])}</div>
+
+            {this.state.showCode &&
+                <div className="code">
+                <div>Code:</div>
+                <code>{iterateOverArray.toString()}</code>
+              </div>
+            }
+          </div>
+
+          <div className="example">
+
+            Add to beginning of array: 
+            <div>Value: [10, 5, 100, 25]</div>
+            <div>Answer: {addToBeginningOfArray([10, 5, 100, 25])}</div>
+
+            {this.state.showCode &&
+                <div className="code">
+                <div>Code:</div>
+                <code>{addToBeginningOfArray.toString()}</code>
+              </div>
+            }
+          </div>
+
+          <div className="example">
+
+            Add to End of array: 
+            <div>Value: [10, 5, 100, 25]</div>
+            <div>Answer: {addToEndOfArray([10, 5, 100, 25])}</div>
+
+            {this.state.showCode &&
+              <div className="code">
+                <div>Code:</div>
+                <code>{addToEndOfArray.toString()}</code>
+              </div>
+            }
+          </div>
+
+          <div className="example">
+            Remove first element from array: 
+            <div>Value: [10, 5, 100, 25]</div>
+            <div>Answer: {removeFirstElementFromArray([10, 5, 100, 25])}</div>
+
+            {this.state.showCode &&
+              <div className="code">
+              <div>Code:</div>
+              <code>{removeFirstElementFromArray.toString()}</code>
+              </div>
+            }
+          </div>
+
+          <div className="example">
+
+            Remove last element from array: 
+            <div>Value: [10, 5, 100, 25]</div>
+            <div>Answer: {removeLastElementFromArray([10, 5, 100, 25])}</div>
+
+            {this.state.showCode &&
+                <div className="code">
+                <div>Code:</div>
+                <code>{removeLastElementFromArray.toString()}</code>
+              </div>
+            }
+          </div>
+        </div>
+
+        <div className="category">
+          Objects:
+
+          <div className="example">
+
+            Add element to object: 
+            <div>Value: {"{'first': 10, 'second': 5, 'third' : 100, 'fourth': 25}"}</div>
+            <div>Answer: {addItemToObject({'first': 10, 'second': 5, 'third' : 100, 'fourth': 25})}</div>
+
+            {this.state.showCode &&
+              <div className="code">
+              <div>Code:</div>
+              <code>{addItemToObject.toString()}</code>
+              </div>
+            }
+
+          </div>
+
+          <div className="example">
+
+            Remove item from object: 
+            <div>Value: {"{'first': 10, 'second': 5, 'third' : 100, 'fourth': 25}"}</div>
+            <div>Answer: {removeItemFromObject({'first': 10, 'second': 5, 'third' : 100, 'fourth': 25})}</div>
+
+            {this.state.showCode &&
+              <div className="code">
+              <div>Code:</div>
+              <code>{removeItemFromObject.toString()}</code>
+              </div>
+            }
+          </div>
+
+          <div className="example">
+
+            Iterate over object: 
+            <div>Value: {"{'first': 10, 'second': 5, 'third' : 100, 'fourth': 25}"}</div>
+            <div>Answer: {iterateOverObject({'first': 10, 'second': 5, 'third' : 100, 'fourth': 25})}</div>
+
+            {this.state.showCode &&
+              <div className="code">
+              <div>Code:</div>
+              <code>{iterateOverObject.toString()}</code>
+              </div>
+            }
+          </div>
+
+        </div>
+
+        <div class="category">
+            <button onClick={this.clickHandler}>Show/Hide Code</button>
         </div>
       </div>
-
-      <div className="category">
-        Arrays
-        <div className="example">
-
-          Sort by largest in array:
-          <div>Value: ["We", "I", "Them", "You"]</div>
-          <div>Answer: {sortByAlphabeticalAscInArray(["We", "I", "Them", "You"])}</div>
-        </div>
-        
-        <div className="example">
-
-          Sort by smallest in array: 
-          <div>Value: ["We", "I", "Them", "You"]</div>
-          <div>Answer: {sortByAlphabeticalDscInArray(["We", "I", "Them", "You"])}</div>
-        </div>
-
-        <div className="example">
-
-          Sort array numbers smallest to largest: 
-          <div>Value: [10, 5, 100, 25]</div>
-          <div>Answer: {sortArrayNumbersSmallestToLargest([10, 5, 100, 25])}</div>
-        </div>
-
-        <div className="example">
-
-          Sort array numbers largest to smallest: 
-          <div>Value: [10, 5, 100, 25]</div>
-          <div>Answer: {sortArrayNumbersLargestToSmallest([10, 5, 100, 25])}</div>
-        </div>
-
-        <div className="example">
-
-          Iterate over array: 
-          <div>Value: [10, 5, 100, 25]</div>
-          <div>Answer: {iterateOverArray([10, 5, 100, 25])}</div>
-        </div>
-
-        <div className="example">
-
-          Add to beginning of array: 
-          <div>Value: [10, 5, 100, 25]</div>
-          <div>Answer: {addToBeginningOfArray([10, 5, 100, 25])}</div>
-        </div>
-
-        <div className="example">
-
-          Add to End of array: 
-          <div>Value: [10, 5, 100, 25]</div>
-          <div>Answer: {addToEndOfArray([10, 5, 100, 25])}</div>
-        </div>
-
-        <div className="example">
-
-          Remove first element from array: 
-          <div>Value: [10, 5, 100, 25]</div>
-          <div>Answer: {removeFirstElementFromArray([10, 5, 100, 25])}</div>
-        </div>
-
-        <div className="example">
-
-          Remove last element from array: 
-          <div>Value: [10, 5, 100, 25]</div>
-          <div>Answer: {removeLastElementFromArray([10, 5, 100, 25])}</div>
-        </div>
-      </div>
-
-      <div className="category">
-        Objects:
-
-        <div className="example">
-
-          Add element to object: 
-          <div>Value: {"{'first': 10, 'second': 5, 'third' : 100, 'fourth': 25}"}</div>
-          <div>Answer: {addItemToObject({'first': 10, 'second': 5, 'third' : 100, 'fourth': 25})}</div>
-        </div>
-
-        <div className="example">
-
-          Remove last element from object: 
-          <div>Value: {"{'first': 10, 'second': 5, 'third' : 100, 'fourth': 25}"}</div>
-          <div>Answer: {removeItemFromObject({'first': 10, 'second': 5, 'third' : 100, 'fourth': 25})}</div>
-        </div>
-
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
